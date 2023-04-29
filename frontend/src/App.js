1b05cdf481bf3c300cd1dab6c08e0fa34025e564
Ashish Shukla <shukla21ashish@gmail.com>
2023-04-29 14:16:17 +0530

    initial changes

---
+++ frontend/src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        <input />
+        <input />
+        <button>update</button>
+      </div>
+      <div>
+        <table>
+          <thead>
+            <th>val1</th>
+            <th>val2</th>
+          </thead>
+          <tbody>
+            <td>hello</td>
+            <td>Ashish </td>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/data.js
@@ -0,0 +1,1 @@
+export const data = [{ val: 1 }, { val: 2 }, { val: 3 }, { val: 4 }];
